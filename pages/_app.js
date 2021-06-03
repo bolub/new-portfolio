9fe2f45d5997f69966f67ea72938b20ff1db817e
bolub <abiol5202@gmail.com>
2021-06-04 00:26:55 +0100

    add new font

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,10 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />

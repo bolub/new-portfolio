f59444eb71cb1403e3a9bab7726ce25cf0cb783c
bolub <abiol5202@gmail.com>
2021-08-16 14:04:58 +0100

    chnage double to single quote

--- chakra/index.js
+++ chakra/index.js
@@ -1,52 +1,52 @@
-import { extendTheme } from "@chakra-ui/react";
-import { brand } from "./colors";
-import { components } from "./components";
-import { config } from "./config";
+import { extendTheme } from '@chakra-ui/react';
+import { brand } from './colors';
+import { components } from './components';
+import { config } from './config';
 
 let defaultHeading, defaultBody, styles, colors, colorMode;
 
-if (typeof window !== "undefined") {
-  defaultHeading = localStorage.getItem("fontHeading") || "Space Grotesk";
-  defaultBody = localStorage.getItem("fontBody") || "Jost";
-  colorMode = localStorage.getItem("chakra-ui-color-mode");
+if (typeof window !== 'undefined') {
+  defaultHeading = localStorage.getItem('fontHeading') || 'Space Grotesk';
+  defaultBody = localStorage.getItem('fontBody') || 'Jost';
+  colorMode = localStorage.getItem('chakra-ui-color-mode');
 
   styles = {
     global: {
-      "html, body": {
+      'html, body': {
         fontFamily: defaultBody,
-        scrollBehavior: "smooth",
+        scrollBehavior: 'smooth',
       },
 
-      "h1, h2, h3, h4, h5, h6": {
+      'h1, h2, h3, h4, h5, h6': {
         fontFamily: defaultHeading,
       },
 
       code: {
-        width: "100%",
-        display: "block",
-        overflowWrap: "break-word",
-        whiteSpace: "pre-wrap",
-        background: "#f2f2f2",
-        fontSize: "14px",
-        color: colorMode === "dark" && "black",
+        width: '100%',
+        display: 'block',
+        overflowWrap: 'break-word',
+        whiteSpace: 'pre-wrap',
+        background: '#f2f2f2',
+        fontSize: '14px',
+        color: colorMode === 'dark' && 'black',
         p: 3,
       },
 
-      ".rs-container": {
-        display: "flex",
+      '.rs-container': {
+        display: 'flex',
         // bg: "brand.500",
-        justifyContent: "center",
+        justifyContent: 'center',
       },
 
-      ".rs-play, .rs-stop, .rs-pause, .rs-resume": {
-        width: "40px",
-        height: "40px",
-        borderRadius: "100px !important",
-        padding: "4px",
-        marginRight: "10px",
-        backgroundColor: "#f2f2f2 !important",
-        color: "#cb1a41 !important",
-        border: "1px solid #cb1a41 !important",
+      '.rs-play, .rs-stop, .rs-pause, .rs-resume': {
+        width: '40px',
+        height: '40px',
+        borderRadius: '100px !important',
+        padding: '4px',
+        marginRight: '10px',
+        backgroundColor: '#f2f2f2 !important',
+        color: '#cb1a41 !important',
+        border: '1px solid #cb1a41 !important',
       },
     },
   };
@@ -54,40 +54,40 @@ if (typeof window !== "undefined") {
   colors = {
     brand,
     red: {
-      50: "#ffe4ed",
-      100: "#fbb9c8",
-      200: "#f28da4",
-      300: "#ec607f",
-      400: "#e5345b",
-      500: "#cb1a41",
-      600: "#9f1233",
-      700: "#730b23",
-      800: "#470415",
-      900: "#1e0006",
+      50: '#ffe4ed',
+      100: '#fbb9c8',
+      200: '#f28da4',
+      300: '#ec607f',
+      400: '#e5345b',
+      500: '#cb1a41',
+      600: '#9f1233',
+      700: '#730b23',
+      800: '#470415',
+      900: '#1e0006',
     },
     blue: {
-      50: "#e5eeff",
-      100: "#b9cbfb",
-      200: "#8da8f3",
-      300: "#6185eb",
-      400: "#3563e4",
-      500: "#1b49ca",
-      600: "#12399e",
-      700: "#0a2972",
-      800: "#031847",
-      900: "#00081d",
+      50: '#e5eeff',
+      100: '#b9cbfb',
+      200: '#8da8f3',
+      300: '#6185eb',
+      400: '#3563e4',
+      500: '#1b49ca',
+      600: '#12399e',
+      700: '#0a2972',
+      800: '#031847',
+      900: '#00081d',
     },
     yellow: {
-      50: "#fff4dc",
-      100: "#fee2b0",
-      200: "#fbce82",
-      300: "#f8bb52",
-      400: "#f6a723",
-      500: "#dc8e09",
-      600: "#ab6e04",
-      700: "#7b4f01",
-      800: "#4a2f00",
-      900: "#1c0f00",
+      50: '#fff4dc',
+      100: '#fee2b0',
+      200: '#fbce82',
+      300: '#f8bb52',
+      400: '#f6a723',
+      500: '#dc8e09',
+      600: '#ab6e04',
+      700: '#7b4f01',
+      800: '#4a2f00',
+      900: '#1c0f00',
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,82 @@
 // chakra
-import { Box, Flex, chakra, Text } from "@chakra-ui/react";
+import { Box, Flex, chakra, Text } from '@chakra-ui/react';
 
 // utils
-import { generalPaddingX } from "../utils/chakra";
+import { generalPaddingX } from '../utils/chakra';
 
 // componentss
-import HomeIllustration from "./../svg/HomeIllustration";
-import CustomLink from "../components/UI/CustomLink";
-import CustomSeo from "../components/Layout/Seo";
+import HomeIllustration from './../svg/HomeIllustration';
+import CustomLink from '../components/UI/CustomLink';
+import CustomSeo from '../components/Layout/Seo';
 
 export default function Home() {
   return (
     <>
-      <CustomSeo title="Home" />
+      <CustomSeo title='Home' />
 
       <chakra.header
-        h={{ base: "100%", md: "80vh" }}
-        alignItems="center"
-        d="flex"
-        py={{ base: "10", md: 0 }}
+        h={{ base: '100%', md: '80vh' }}
+        alignItems='center'
+        d='flex'
+        py={{ base: '10', md: 0 }}
       >
         <Flex
-          w="100%"
-          flexDir={{ base: "column", md: "row" }}
+          w='100%'
+          flexDir={{ base: 'column', md: 'row' }}
           px={generalPaddingX}
         >
           <Box
             mb={{ base: 0, md: 0 }}
-            w={{ base: "100%", md: "45%" }}
-            my="auto"
+            w={{ base: '100%', md: '45%' }}
+            my='auto'
           >
-            <chakra.h1 fontWeight={700} fontSize={{ base: "3xl", md: "4xl" }}>
+            <chakra.h1 fontWeight={700} fontSize={{ base: '3xl', md: '4xl' }}>
               Hello, I'm
-              <Text as="span" color="brand.500">
-                {" "}
+              <Text as='span' color='brand.500'>
+                {' '}
                 Bolu
               </Text>
             </chakra.h1>
 
-            <Text mt={4} mb={6} fontSize="17px">
+            <Text mt={4} mb={6} fontSize='17px'>
               Welcome to my ......., well you know.
             </Text>
 
-            <Text mb={6} fontSize="17px">
+            <Text mb={6} fontSize='17px'>
               Honestly doing this was a pain, but still did it anyways, gotta
               have an online presence and all
             </Text>
 
-            <Text mb={6} fontSize="17px">
-              Gonna be doing a bit of{" "}
-              <CustomLink active href="/blog">
+            <Text mb={6} fontSize='17px'>
+              Gonna be doing a bit of{' '}
+              <CustomLink active href='/blog'>
                 blogging
               </CustomLink>
-              , probably{" "}
-              <CustomLink active href="/cooking">
+              , probably{' '}
+              <CustomLink active href='/cooking'>
                 cooking
               </CustomLink>
-              , adding{" "}
-              <CustomLink active href="/resources">
+              , adding{' '}
+              <CustomLink active href='/resources'>
                 useful stuff
               </CustomLink>
-              , and also uploading my{" "}
-              <CustomLink active href="/projects">
+              , and also uploading my{' '}
+              <CustomLink active href='/projects'>
                 portfolio.
               </CustomLink>
             </Text>
 
-            <Text mb={6} fontSize="17px">
+            <Text mb={6} fontSize='17px'>
               Not sure about how i feel about a logo for now though, an emoji
               would suffice for now.
             </Text>
           </Box>
 
           {/* Illustration */}
-          <Box w={{ base: "100%", md: "55%" }} my="auto" d="flex">
+          <Box w={{ base: '100%', md: '55%' }} my='auto' d='flex'>
             <HomeIllustration
-              m="auto"
-              height="350"
+              m='auto'
+              height='350'
               // borderTop="3px solid black"
               // borderLeft="3px solid black"
               // borderRight="3px solid black"

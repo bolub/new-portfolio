e0ffe6c0f7c94229827ddebd6098b1eb093102c5
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-15 10:41:55 +0300

    pause singlee on prod

--- components/projects/SingleProject.tsx
+++ components/projects/SingleProject.tsx
@@ -9,50 +9,47 @@ import {
   Tooltip,
   AspectRatio,
   Button,
-} from '@chakra-ui/react';
-import Image from 'next/image';
+} from "@chakra-ui/react";
+import Image from "next/image";
 import {
   BsCodeSlash,
   BsBezier,
   BsLayoutTextWindowReverse,
-} from 'react-icons/bs';
-import { VideoPlayer } from '../VideoPlayer';
-import CustomLink from '../UI/CustomLink';
+} from "react-icons/bs";
+import { VideoPlayer } from "../VideoPlayer";
+import CustomLink from "../UI/CustomLink";
 
-const SingleProject = ({ data }: {
-  data: any
-}) => {
-
+const SingleProject = ({ data }: { data: any }) => {
   return (
     <Box>
       {data.imageUrl && (
-        <Box h={{ base: '300px', md: '500px' }} pos='relative'>
+        <Box h={{ base: "300px", md: "500px" }} pos="relative">
           <Image
             src={data.imageUrl}
-            objectFit={'cover'}
-            layout='fill'
-            placeholder='blur'
+            objectFit={"cover"}
+            layout="fill"
+            placeholder="blur"
             alt={data?.title}
           />
         </Box>
       )}
 
       {data.videoUrl && (
-        <AspectRatio height={{ base: '300px', md: '500px' }} ratio={1}>
+        <AspectRatio height={{ base: "300px", md: "500px" }} ratio={1}>
           <VideoPlayer src={data.videoUrl} />
         </AspectRatio>
       )}
 
-      <Wrap display={{ base: 'flex', md: 'flex' }} mt={5}>
-        {data.tags?.map((tag: {name: string; id: string}) => {
+      <Wrap display={{ base: "flex", md: "flex" }} mt={5}>
+        {data.tags?.map((tag: { name: string; id: string }) => {
           return (
             <Tag
               key={tag?.id}
-              cursor='pointer'
-              colorScheme='brand'
-              variant='subtle'
-              borderRadius='full'
-              size='md'
+              cursor="pointer"
+              colorScheme="brand"
+              variant="subtle"
+              borderRadius="full"
+              size="md"
             >
               {tag?.name}
             </Tag>
@@ -63,22 +60,22 @@ const SingleProject = ({ data }: {
       <Flex mt={5}>
         <chakra.h3
           mt={2}
-          fontWeight={'medium'}
-          fontSize={{ base: 'xl', md: '2xl' }}
+          fontWeight={"medium"}
+          fontSize={{ base: "xl", md: "2xl" }}
         >
           {data.title}
         </chakra.h3>
 
-        <HStack ml='auto'>
+        <HStack ml="auto">
           {data.deignUrl && (
-            <Tooltip label='Design' aria-label='Design'>
+            <Tooltip label="Design" aria-label="Design">
               <IconButton
-                as='a'
+                as="a"
                 href={data.deignUrl}
-                target='_blank'
-                aria-label='Deign'
-                colorScheme={'brand'}
-                variant='ghost'
+                target="_blank"
+                aria-label="Deign"
+                colorScheme={"brand"}
+                variant="ghost"
               >
                 <BsBezier />
               </IconButton>
@@ -86,14 +83,14 @@ const SingleProject = ({ data }: {
           )}
 
           {data.sourceCode && (
-            <Tooltip label='Source Code' aria-label='Source Code'>
+            <Tooltip label="Source Code" aria-label="Source Code">
               <IconButton
-                as='a'
+                as="a"
                 href={data.sourceCode}
-                target='_blank'
-                aria-label='Source Code'
-                colorScheme={'brand'}
-                variant='ghost'
+                target="_blank"
+                aria-label="Source Code"
+                colorScheme={"brand"}
+                variant="ghost"
               >
                 <BsCodeSlash />
               </IconButton>
@@ -101,14 +98,14 @@ const SingleProject = ({ data }: {
           )}
 
           {data.url && (
-            <Tooltip label='Live url' aria-label='Live url'>
+            <Tooltip label="Live url" aria-label="Live url">
               <IconButton
-                as='a'
+                as="a"
                 href={data.url}
-                target='_blank'
-                aria-label='Live url'
-                colorScheme={'brand'}
-                variant='ghost'
+                target="_blank"
+                aria-label="Live url"
+                colorScheme={"brand"}
+                variant="ghost"
               >
                 <BsLayoutTextWindowReverse />
               </IconButton>
@@ -117,15 +114,14 @@ const SingleProject = ({ data }: {
         </HStack>
       </Flex>
 
-      <chakra.p fontSize={'lg'} mt={3} mb={8}>
+      <chakra.p fontSize={"lg"} mt={3} mb={8}>
         {data.description}
       </chakra.p>
 
-   
-
+      {/* 
       <CustomLink href={`/projects/${data?.id}`} active>
         Read more
-      </CustomLink>
+      </CustomLink> */}
     </Box>
   );
 };

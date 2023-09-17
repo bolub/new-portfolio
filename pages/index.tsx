850b32b5c5b20aed4ef3dc1a6a18c5d6a9e06839
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-17 09:13:22 +0300

    fetch projects home data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import { chakra } from '@chakra-ui/react';
+import { chakra } from "@chakra-ui/react";
 
-import CustomSeo from '../components/Layout/Seo';
-import Header from '../components/homePage/Header';
-import About from '../components/homePage/About';
+import CustomSeo from "../components/Layout/Seo";
+import Header from "../components/homePage/Header";
+import About from "../components/homePage/About";
+import { trpcHelpers } from "../server/routers/_app";
 
 export default function Home() {
   return (
     <>
-      <CustomSeo title='Home' />
+      <CustomSeo title="Home" />
 
       <chakra.main>
         <Header />
@@ -15,4 +16,16 @@ export default function Home() {
       </chakra.main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  await trpcHelpers.project.some.prefetch({
+    quantity: 2,
+  });
+
+  return {
+    props: {
+      trpcState: trpcHelpers.dehydrate(),
+    },
+  };
 }

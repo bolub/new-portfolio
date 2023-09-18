8b621f72db51c05c8c64610ddd415fa79a0c4e71
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-19 02:34:27 +0300

    update

--- components/homePage/Header.js
+++ components/homePage/Header.js
@@ -53,7 +53,7 @@ const Header = () => {
 
           <CustomButton
             as="a"
-            href="https://www.linkedin.com/in/abiola-boluwatife-2569a915b/"
+            href="https://www.linkedin.com/in/boluseyi"
             mb={10}
             target="__blank"
             size="lg"

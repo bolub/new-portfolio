f1f1ee5e0b65f3f82dd9427de2ccf8bdf8c1b585
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-03-21 19:45:29 +0200

    Added a default url cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,16 @@ export const metadata: Metadata = {
   title: "Boluwatife Abiola",
   description:
     "Welcome to my space. Find out all about me here, might get a little weird though",
+  openGraph: {
+    images: [
+      {
+        url: "https://res.cloudinary.com/bolub/image/upload/v1623525073/Group_1_1.png",
+        height: 552,
+        width: 318,
+        alt: "Cover Image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({

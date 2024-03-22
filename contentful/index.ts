9b08a911467bb5f606152ac93cf408bc8aa9c7df
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-03-22 22:47:50 +0200

    create preview client

--- contentful/index.ts
+++ contentful/index.ts
@@ -4,3 +4,10 @@ export const client = createClient({
   space: process.env.NEXT_PUBLIC_SPACE_ID as string,
   accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN as string,
 });
+
+export const previewClient = createClient({
+  space: process.env.NEXT_PUBLIC_SPACE_ID as string,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTUL_PREVIEW_API_TOKEN as string,
+  host: "preview.contentful.com",
+  // preview.eu.contentful.com
+});
